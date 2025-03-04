caf93eb7aada22d567e33a21c184a426c3cebe84
dzaki236 <dzakiahnafz@gmail.com>
2025-03-04 14:04:29 +0700

    update

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ import express from "express";
 import * as path from "path";
 import { route } from "./app/routes/wilayah.route.js";
 import cors from "cors";
-import serverless from "serverless-http";
 const App = express();
-const port = 8080;
+const port = 5000;
 App.use(cors());
 App.use(express.json());
-App.use('/.netlify/functions/api', route);
+App.use('/api', route);
 App.all('*', (req, res) => {
     res.status(404).json({
         "message": "Request failed with status code 404",
@@ -15,6 +14,4 @@ App.all('*', (req, res) => {
         "status": 404
     });
 });
-// export.handler
-// App.listen(port, () => console.log(`Server running on port ${port}`));
-module.exports.handler = serverless(App);
+App.listen(port, () => console.log(`Server running on port ${port}`));
